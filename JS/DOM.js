86fbf06c4c20716f8dd925e98d21ad4e2ee640c2
Vitor Gomes <60221263+Vitor-WSG@users.noreply.github.com>
2020-09-22 17:41:32 -0300

    Manipulação DOM
    Manipulação DOM

---
+++ JS/DOM.js
@@ -0,0 +1,12 @@
+// var element = document.querySelector('h1');
+// element.textContent='Joao da Silva';
+
+var p1 = document.querySelector('#p1');
+var p2 = document.querySelector('#p2');
+var p3 = document.querySelector('#p3');
+var p4 = document.querySelector('#p4');
+
+p1.classlist.add('LUA');
+p2.classlist.add('MARTE');
+p3.classList.add('SATURNO');
+p4.classList.add('JUPTER');
